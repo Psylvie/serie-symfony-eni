d85fff2362d64fa4131410774a08f29106f9f78f
Sylvie <peuzin.sylvie.sp@gmail.com>
2022-09-06 16:37:45 +0200

    module 5

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,10 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends AbstractController
-{ /**
- *  @Route("/", name="main_home")
- */
+class MainController extends AbstractController{
+    /**
+    *  @Route("/", name="main_home")
+     */
     public function home() {
         return $this->render("main/home.html.twig");
        }
--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Serie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route('/series', name="serie_")
+ * @Route("/series", name="serie_")
  */
+
 class SerieController extends AbstractController
 {
     /**
@@ -29,9 +33,44 @@ class SerieController extends AbstractController
 }
 
     /**
-     * @Route('/create', name="create")
+     * @Route("/create", name="create")
      */
 public function create():Response{
         return $this->render('serie/create.html.twig');
 }
+
+  /**
+   * @Route("/demo",name="em-demo")
+   */
+public function demo(EntityManagerInterface $entityManager):Response{
+
+    $serie = new Serie();
+    $serie->setName('pif');
+    $serie->setBackdrop('dafsd');
+    $serie->setPoster('dafsd');
+    $serie->setDateCreated(new \DateTime());
+    $serie->setFirstAirDate(new \DateTime("-1 year"));
+    $serie->setLastAirDate(new \DateTime("-6 month"));
+    $serie->setGenres('drama');
+    $serie->setOverview('bla bla bla');
+    $serie->setPopularity(123.00);
+    $serie->setVote(8.2);
+    $serie->setStatus('Canceled');
+    $serie->setTmdbId(329432);
+
+    dump($serie);
+
+    // $entityManager = $this->getDoctrine()->getManager()
+
+    $entityManager->persist($serie);
+    $entityManager->flush();
+
+    dump($serie);
+    //$entityManager->remove($serie);
+    $serie->setGenres('comedy');
+    $entityManager->flush();
+
+    return $this->render('serie/create.html.twig');
+}
+
 }
